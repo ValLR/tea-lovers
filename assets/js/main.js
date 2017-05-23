8827770d358c66a87051ed774aa733cd15cce2b9
valentina lopez <valentina lopez>
2017-05-23 05:52:41 -0400

    validaciones y diseño del newsletter

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
-alert("no")
+var sub = document.getElementById("suscribirse");
+sub.addEventListener("click", suscribe);
+
+function suscribe (){
+	var name = document.getElementById("name").value;
+	var email = document.getElementById("email").value;
+	var address = document.getElementById("address").value;
+	var select = document.getElementById("list").selectedIndex;
+
+	if(name == "" || /^[a-zA-Z]*$/.test(name) == false ||email == "" || /\S+@\S+\.\S+/.test(email) == false ||  address == "" || select == 0){
+  		alert("ERROR, campo inválido");
+  	}else{
+  		alert("Muchas gracias por tu suscripción, " + name);
+  	}
+}
+
+
+	
